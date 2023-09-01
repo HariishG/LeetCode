e2513193040b1191b7789fa7091491654e3eaae8
Hariish Ganesan <101449215+HariishG@users.noreply.github.com>
2023-09-01 20:31:23 +0530

    Time: 35 ms (88.54%), Space: 16.4 MB (5.01%) - LeetHub

--- 1915-check-if-one-string-swap-can-make-strings-equal/1915-check-if-one-string-swap-can-make-strings-equal.py
+++ 1915-check-if-one-string-swap-can-make-strings-equal/1915-check-if-one-string-swap-can-make-strings-equal.py
@@ -5,18 +5,16 @@ class Solution:
         ind1=0
         ind2=0
         for i in range(len(s1)):
-            if s1[i]==s2[i]:
-                continue
-            ind1=i
-            i+=1
-            break
+            if s1[i]!=s2[i]:
+                ind1=i
+                i+=1
+                break   
         else:
             return True
         for j in range(i,len(s1)):
-            if s1[j]==s2[j]:
-                continue
-            ind2=j
-            break
+            if s1[j]!=s2[j]:
+                ind2=j
+                break
         if s1[ind1]==s2[ind2] and s1[ind2]==s2[ind1] and s1[ind2+1:]==s2[ind2+1:]:
             return True
         return False
