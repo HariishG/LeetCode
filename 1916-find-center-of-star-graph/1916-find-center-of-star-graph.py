08b118a20ca99e0af2bd559fa82bfb50ef884761
Hariish Ganesan <101449215+HariishG@users.noreply.github.com>
2023-09-19 10:36:26 +0530

    Time: 706 ms (94.85%), Space: 51.9 MB (95.48%) - LeetHub

--- 1916-find-center-of-star-graph/1916-find-center-of-star-graph.py
+++ 1916-find-center-of-star-graph/1916-find-center-of-star-graph.py
@@ -1,7 +1,7 @@
 class Solution:
     def findCenter(self, edges: List[List[int]]) -> int:
         dic=defaultdict(int)
-        for i in edges:
+        for i in edges[:2]:
             dic[i[0]]+=1
             dic[i[1]]+=1
         Keymax = max(dic, key= lambda x: dic[x])
