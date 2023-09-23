9e5eeccb881f1568d564714ff9495df145204a4a
Hariish Ganesan <101449215+HariishG@users.noreply.github.com>
2023-09-23 20:30:38 +0530

    Time: 30 ms (95.22%), Space: 16.3 MB (79.89%) - LeetHub

---
+++ 0017-letter-combinations-of-a-phone-number/0017-letter-combinations-of-a-phone-number.py
@@ -0,0 +1,15 @@
+class Solution:
+    def letterCombinations(self, digits: str) -> List[str]:
+        letters=["abc", "def", "ghi", "jkl", "mno", "pqrs", "tuv", "wxyz"]
+        if digits=="":
+            return[]
+        op=[]
+        def find(rem_dig, curr_seq=""):
+            if len(rem_dig)==1:
+                for i in letters[int(rem_dig)-2]:
+                    op.append(curr_seq+i)
+            else:
+                for i in letters[int(rem_dig[0])-2]:
+                    find(rem_dig[1:], curr_seq+i)
+        find(digits)
+        return op
