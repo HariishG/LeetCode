1bb538f05aaaa7e36bed074912d0f76ea438266d
Hariish Ganesan <101449215+HariishG@users.noreply.github.com>
2023-09-29 08:41:19 +0530

    Time: 176 ms (12.54%), Space: 16.1 MB (99.09%) - LeetHub

--- 0397-integer-replacement/0397-integer-replacement.py
+++ 0397-integer-replacement/0397-integer-replacement.py
@@ -4,10 +4,10 @@ class Solution:
     def findOp(self, num):
         if num<1:
             return float('inf')
-        if num==1:
+        elif num==1:
             return 0
+        elif num%2==0:
+            return 1+self.findOp(num//2)
         else:
-            if num%2==0:
-                return 1+self.findOp(num//2)
             return 1+min(self.findOp(num-1),self.findOp(num+1))
         
