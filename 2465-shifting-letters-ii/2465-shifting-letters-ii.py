03c9bca1602a545fb15f7332b0d73bd0d53f92b6
Hariish Ganesan <101449215+HariishG@users.noreply.github.com>
2023-10-08 11:52:03 +0530

    Time: 1642 ms (20.09%), Space: 41.6 MB (62.05%) - LeetHub

---
+++ 2465-shifting-letters-ii/2465-shifting-letters-ii.py
@@ -0,0 +1,17 @@
+class Solution:
+    def shiftingLetters(self, s: str, shifts: List[List[int]]) -> str:
+        shift_amount=[0 for _ in range(len(s)+1)]
+        for i in shifts:
+            if i[2]==1:
+                shift_amount[i[0]]+=1
+                shift_amount[i[1]+1]-=1
+            else:
+                shift_amount[i[0]]-=1
+                shift_amount[i[1]+1]+=1
+        total=0
+        print(shift_amount)
+        for i in range(len(s)):
+            total+=shift_amount[i]
+            sa=97+(ord(s[i])+total-97)%26
+            s = s[:i] + chr(sa) + s[i+1:]
+        return s
