cf0e60579ffb5831e62d5b1a7717fd02aa14e45a
Hariish Ganesan <101449215+HariishG@users.noreply.github.com>
2024-01-08 11:36:52 +0530

    Time: 402 ms (98.42%), Space: 31.4 MB (8.75%) - LeetHub

--- 1218-longest-arithmetic-subsequence-of-given-difference/1218-longest-arithmetic-subsequence-of-given-difference.py
+++ 1218-longest-arithmetic-subsequence-of-given-difference/1218-longest-arithmetic-subsequence-of-given-difference.py
@@ -4,16 +4,12 @@ class Solution:
         index=defaultdict(lambda : -1)
         dp=[1 for _ in range(n)]
         index[arr[0]]=0
-        ans=1
         for i in range(1,n):
             idx=index[arr[i]-difference]
             index[arr[i]]=i
             if idx==-1:
                 continue
             else:
-                print(22)
                 dp[i]=dp[idx]+1
-                ans=max(ans, dp[idx]+1)
-
-        return ans
+        return max(dp)
             
