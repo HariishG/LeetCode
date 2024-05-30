06d0f342e177f50f2660013364e9c8a491f60cea
Hariish Ganesan <101449215+HariishG@users.noreply.github.com>
2024-05-30 14:20:09 +0530

    Time: 47 ms (67.82%), Space: 16.4 MB (99.43%) - LeetHub

--- 1442-count-triplets-that-can-form-two-arrays-of-equal-xor/1442-count-triplets-that-can-form-two-arrays-of-equal-xor.py
+++ 1442-count-triplets-that-can-form-two-arrays-of-equal-xor/1442-count-triplets-that-can-form-two-arrays-of-equal-xor.py
@@ -2,10 +2,12 @@ class Solution:
     def countTriplets(self, arr: List[int]) -> int:
         ans=0
         n=len(arr)
+        pre=[0]
         for i in range(n):
-            temp=0
-            for j in range(i,n):
-                temp^=arr[j]
-                if temp==0:
-                    ans+=j-i
+            pre.append(pre[-1]^arr[i])
+        
+        for i in range(n+1):
+            for j in range(i+1, n+1):
+                if pre[i]==pre[j]:
+                    ans+=j-i-1
         return ans
