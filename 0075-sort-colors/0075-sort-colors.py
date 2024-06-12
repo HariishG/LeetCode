cd47f98f261abd7d5a39bd03aadb62abd8dbf007
Hariish Ganesan <101449215+HariishG@users.noreply.github.com>
2024-06-12 23:41:49 +0530

    Time: 39 ms (45.53%), Space: 16.5 MB (79.02%) - LeetHub

--- 0075-sort-colors/0075-sort-colors.py
+++ 0075-sort-colors/0075-sort-colors.py
@@ -1,23 +1,13 @@
 class Solution:
     def sortColors(self, nums: List[int]) -> None:
-        for i in range(len(nums)):
-            for j in range(len(nums)-i-1):
-                if nums[j]>nums[j+1]:
-                    nums[j],nums[j+1]=nums[j+1],nums[j]
-        # a=b=c=0
-        # for i in nums:
-        #     if i==0:
-        #         a+=1
-        #     elif i==1:
-        #         b+=1
-        #     else:
-        #         c+=1
-        # for i in range(a):
-        #     nums[i]=0
-        # i+=1
-        # for j in range(b):
-        #     nums[i]=1
-        #     i+=1
-        # for k in range(c):
-        #     nums[i]=2
-        #     i+=1
+        freq=Counter(nums)
+        idx=0
+        for i in range(freq[0]):
+            nums[idx]=0
+            idx+=1
+        for i in range(freq[1]):
+            nums[idx]=1
+            idx+=1
+        for i in range(freq[2]):
+            nums[idx]=2
+            idx+=1
