9c756ca27c1797f2b888cc6a5fdcc10db6124c7e
Hariish Ganesan <101449215+HariishG@users.noreply.github.com>
2024-08-19 11:05:32 +0530

    Time: 56 ms (66.63%), Space: 16.4 MB (99.66%) - LeetHub

--- 0650-2-keys-keyboard/0650-2-keys-keyboard.py
+++ 0650-2-keys-keyboard/0650-2-keys-keyboard.py
@@ -3,7 +3,7 @@ class Solution:
         if n==1:
             return 0
         ans=[i for i in range(n+1)]
-        for i in range(1,n+1):
+        for i in range(2,n+1):
             for j in range(2,n//i+1):
                 ans[i*j]=min(ans[i*j], ans[i]+j)
         return ans[-1]
