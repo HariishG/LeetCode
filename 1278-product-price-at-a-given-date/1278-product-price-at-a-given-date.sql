15143b5c3c810004cc3d94ef376fc1fe5ce85b99
Hariish Ganesan <101449215+HariishG@users.noreply.github.com>
2024-08-21 12:55:43 +0530

    Time: 5139 ms (5.01%), Space: 0B (100%) - LeetHub

--- 1278-product-price-at-a-given-date/1278-product-price-at-a-given-date.sql
+++ 1278-product-price-at-a-given-date/1278-product-price-at-a-given-date.sql
@@ -1,11 +1,4 @@
 # Write your MySQL query statement below
--- SELECT product_id, 
---     IF((SELECT COUNT(change_date) FROM Products WHERE product_id=P.product_id AND change_date="2019-08-16")>0,
---     (SELECT new_price FROM Products WHERE product_id=P.product_id AND change_date="2019-08-16"),
---     IF((SELECT COUNT(change_date) FROM Products WHERE product_id=P.product_id AND change_date<"2019-08-16")>0,
---     (SELECT new_price FROM Products WHERE product_id=P.product_id AND change_date<"2019-08-16" ORDER BY change_date DESC LIMIT 1),
---     10)) AS price FROM Products P GROUP BY product_id
-
 SELECT product_id, 
     IF((SELECT COUNT(change_date) FROM Products WHERE product_id=P.product_id AND change_date<="2019-08-16")>0,
     (SELECT new_price FROM Products WHERE product_id=P.product_id AND change_date<="2019-08-16" ORDER BY change_date DESC LIMIT 1),
