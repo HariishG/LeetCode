8d0865aaaaf5d1765ffa561bc9b46bd8b5ffca1d
Hariish Ganesan <101449215+HariishG@users.noreply.github.com>
2024-08-22 09:57:31 +0530

    Time: 223 ms (82.97%), Space: 19.6 MB (18.84%) - LeetHub

--- 0664-strange-printer/0664-strange-printer.py
+++ 0664-strange-printer/0664-strange-printer.py
@@ -1,19 +1,17 @@
 class Solution:
     def strangePrinter(self, s: str) -> int:
         s = "".join(char for char, _ in itertools.groupby(s))
-        memo = {}
-        def _minimum_turns(start, end):
+        dp = {}
+        def func(start, end):
             if start > end:
                 return 0
-            if (start, end) in memo:
-                return memo[(start, end)]
-            min_turns = 1 + _minimum_turns(start + 1, end)
-            for k in range(start + 1, end + 1):
+            if (start, end) in dp:
+                return dp[(start, end)]
+            Min = 1+func(start+1, end)
+            for k in range(start+1, end+1):
                 if s[k] == s[start]:
-                    turns_with_match = _minimum_turns(
-                        start, k - 1
-                    ) + _minimum_turns(k + 1, end)
-                    min_turns = min(min_turns, turns_with_match)
-            memo[(start, end)] = min_turns
-            return min_turns
-        return _minimum_turns(0, len(s) - 1)
+                    temp = func(start, k-1) + func(k+1, end)
+                    Min = min(Min, temp)
+            dp[(start, end)] = Min
+            return Min
+        return func(0, len(s) - 1)
