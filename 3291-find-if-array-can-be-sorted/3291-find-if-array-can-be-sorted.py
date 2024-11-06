66096d46b4951eef7c7400b9b37d336e340b9344
Hariish Ganesan <101449215+HariishG@users.noreply.github.com>
2024-11-06 21:46:35 +0530

    Time: 15 ms (39.09%), Space: 16.7 MB (23.35%) - LeetHub

--- 3291-find-if-array-can-be-sorted/3291-find-if-array-can-be-sorted.py
+++ 3291-find-if-array-can-be-sorted/3291-find-if-array-can-be-sorted.py
@@ -1,13 +1,25 @@
 class Solution:
     def canSortArray(self, nums: List[int]) -> bool:
         n=len(nums)
-        for i in range(n):
-            nums[i]=[nums[i], bin(nums[i]).count('1')]
-        for i in range(n):
-            for j in range(n-i-1):
-                if nums[j][0]>nums[j+1][0]:
-                    if nums[j][1]==nums[j+1][1]:
-                        nums[j],nums[j+1]=nums[j+1],nums[j]
-                    else:
-                        return False
+        cnt=bin(nums[0]).count('1')
+        Min=nums[0]
+        Max=nums[0]
+        prev_max=0
+        i=0
+        while i<n:
+            while i<n and bin(nums[i]).count('1')==cnt:
+                Min=min(Min, nums[i])
+                Max=max(Max, nums[i])
+                i+=1
+            if Min<prev_max:
+                return False
+            if i<n:
+                prev_max=Max
+                cnt=bin(nums[i]).count('1')
+                Min=nums[i]
+                Max=nums[i]
+        if Min<prev_max:
+            return False
         return True
+
+            
