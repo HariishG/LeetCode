a698d43881719570fae14293c7f15fe4c1a6a47d
Hariish Ganesan <101449215+HariishG@users.noreply.github.com>
2024-11-08 18:46:50 +0530

    Time: 79 ms (20.53%), Space: 32.6 MB (79.65%) - LeetHub

--- 1940-maximum-xor-for-each-query/1940-maximum-xor-for-each-query.py
+++ 1940-maximum-xor-for-each-query/1940-maximum-xor-for-each-query.py
@@ -1,23 +1,10 @@
 class Solution:
     def getMaximumXor(self, nums: List[int], maximumBit: int) -> List[int]:
-        def reverseBits(n):
-            rev = 0
-            while (n > 0):
-                rev = rev << 1
-                if (n & 1 == 1):
-                    rev = rev ^ 1
-                n = n >> 1
-            return rev
         n=len(nums)
-        res=[0 for _ in range(n)]
+        res=[]
         pre=0
         for i in range(n):
             pre^=nums[i]
-            k=1
-            for j in range(maximumBit):
-                k<<=1
-                if not(pre>>j&1):
-                    k|=1
-            res[-i-1]=reverseBits(k)>>1
-        return res
+            res.append(~pre&((2**maximumBit)-1))
+        return res[::-1]
         
