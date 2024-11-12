88be04e2bba61c7f14bfea1a1c9aaf082f6a8b48
Hariish Ganesan <101449215+HariishG@users.noreply.github.com>
2024-11-12 11:43:16 +0530

    Time: 410 ms (6.1%), Space: 61.6 MB (98.39%) - LeetHub

---
+++ 2179-most-beautiful-item-for-each-query/2179-most-beautiful-item-for-each-query.py
@@ -0,0 +1,22 @@
+class Solution:
+    def maximumBeauty(self, items: List[List[int]], queries: List[int]) -> List[int]:
+        def binarySearch(l, r, q):
+            b=0
+            while l<=r:
+                m=(l+r)//2
+                if items[m][0]<=q:
+                    b=max(b, items[m][1])
+                    l=m+1
+                elif items[m][0]>q:
+                    r=m-1
+            return b
+            
+
+        n=len(items)
+        items.sort(key=lambda x:(x[0],x[1]))
+        for i in range(1,n):
+            items[i][1]=max(items[i-1][1],items[i][1])
+        res=[]
+        for i in queries:
+            res.append(binarySearch(0,n-1,i))
+        return res
