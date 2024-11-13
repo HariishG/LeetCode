7da0b862fed6509d4b0099a90e3ebfa851a1cdaa
Hariish Ganesan <101449215+HariishG@users.noreply.github.com>
2024-11-13 19:37:00 +0530

    Time: 60 ms (22.94%), Space: 28.1 MB (81.45%) - LeetHub

--- 0041-first-missing-positive/0041-first-missing-positive.py
+++ 0041-first-missing-positive/0041-first-missing-positive.py
@@ -7,7 +7,7 @@ class Solution:
         for i in range(n):
             if abs(nums[i])<=n and not(nums[abs(nums[i])-1]<0):
                 nums[abs(nums[i])-1]*=-1
-        print(nums)
+        
         for i in range(n):
             if not(nums[i]<0):
                 return i+1
